77ab1b8d4c98f4b4c7d337f557eaf83643523d00
jasmineespejo <161428590+jasmineespejo@users.noreply.github.com>
2024-10-25 18:51:50 +1100

    Completed PCA and showcased key points:

--- SCRIPTS_PCA/pca_main_functions.py
+++ SCRIPTS_PCA/pca_main_functions.py
@@ -10,6 +10,7 @@ def load_interpolated_dos(filepath):
     """
     Loads the interpolated DOS data from a given text file.
     Assumes the file contains a nx1001 matrix, where:
+        - the first row corresponds to the energy values
         - the first column corresponds to molecule names
         - the remaining 1000 columns are DOS data for various energy values 
             (with the range adjusted for the max/min of all molecules)
@@ -21,42 +22,39 @@ def load_interpolated_dos(filepath):
         
         # Separate the molecule names and DOS data
         molecule_names = data.iloc[:, 0] # First column (molecule names)
-        dos_data = data.iloc[:, 1:].values  # Remaining columns (DOS data)
+        dos_data = data.iloc[:, 1:] # Remaining columns (DOS data)
+        # print(f"the dos data: {dos_data}")
         return molecule_names, dos_data
+    
     except FileNotFoundError:
         print(f"File not found: {filepath}")
         return None, None
+    
     except Exception as e:
         print(f"Error reading {filepath}: {e}")
 
-def perform_PCA(dos_data):
+def perform_PCA(sample_data):
 
-    try:
-        header_features = dos_data[0,:]
-        sample_data = dos_data[1:,:]
+    base_dir=os.path.join(os.getcwd(), 'DOSCAR_files')
 
-        base_dir=os.path.join(os.getcwd(), 'DOSCAR_files')
+    # Standardise/scale data
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(sample_data)
 
-        # Standardise/scale data
-        scaler = StandardScaler()
-        scaled_data = scaler.fit_transform(sample_data)
+    # Apply PCA
+    n_samples = scaled_data.shape[0]
+    pca = PCA(n_components=n_samples)
+    principal_components = pca.fit_transform(scaled_data)
 
-        # Apply PCA
-        n_samples = scaled_data.shape[0]
-        pca = PCA(n_components=n_samples)
-        principal_components = pca.fit_transform(scaled_data)
+    # Save PCA results (create the file if it doesn't exist)
+    filepath = os.path.join(os.getcwd(),'pca_data.txt')
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
-        # Save PCA results (create the file if it doesn't exist)
-        filepath = os.path.join(os.getcwd(),'pca_data.txt')
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    np.savetxt(filepath, principal_components, delimiter=',', comments='')
+    print(f"PCA results saved at: {filepath}")
 
-        np.savetxt(filepath, principal_components, delimiter=',', header='PC1,PC2', comments='')
-        print(f"PCA results saved at: {filepath}")
+    return pca, scaled_data
 
-        return pca, scaled_data
-
-    except Exception as e:
-        print(f"Error performing {dos_data}: {e}")
 
 if __name__ == "__main__":
     # Check if the the directory path argument is provided
